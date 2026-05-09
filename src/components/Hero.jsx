cee0b5aac5f2926a4e00c1c8e7e1c9f18d0e3c96
Anthony Porquet <anthony.porquet@hotmail.fr>
2026-05-09 11:36:20 +0200

    mise en place

---
+++ src/components/Hero.jsx
@@ -0,0 +1,10 @@
+function Hero() {
+  return (
+    <section className="hero">
+      <h1>Développeur Web</h1>
+      <p>Bienvenue sur mon portfolio React</p>
+    </section>
+  );
+}
+
+export default Hero;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,15 @@
+function Navbar() {
+  return (
+    <nav className="navbar">
+      <h2>Anthony</h2>
+
+      <ul>
+        <li><a href="#about">About</a></li>
+        <li><a href="#projects">Projets</a></li>
+        <li><a href="#contact">Contact</a></li>
+      </ul>
+    </nav>
+  );
+}
+
+export default Navbar;
