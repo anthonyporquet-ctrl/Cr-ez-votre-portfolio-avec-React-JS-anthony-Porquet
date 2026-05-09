1b32dcd3ef8200d3f538c4e59e070237ed0a93c3
Anthony Porquet <anthony.porquet@hotmail.fr>
2026-05-09 11:58:14 +0200

    mise en page 2

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,19 +1,11 @@
 import Navbar from "../components/Navbar";
 import Hero from "../components/Hero";
-import About from "../components/About";
-import Projects from "../components/Projects";
-import Contact from "../components/Contact";
-import Footer from "../components/Footer";
 
 function Home() {
   return (
     <>
       <Navbar />
       <Hero />
-      <About />
-      <Projects />
-      <Contact />
-      <Footer />
     </>
   );
 }
