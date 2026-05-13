aed9964fc4badaf63168c62b52d087f1b4b3a937
Anthony Porquet <anthony.porquet@hotmail.fr>
2026-05-13 11:57:46 +0200

    Modal v2

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,31 +1,19 @@
 import { createPortal } from "react-dom";
+import "./Modal.css";
 
 export default function Modal({ open, close, children }) {
   if (!open) return null;
 
   return createPortal(
-    <div
-      onClick={close}
-      style={{
-        position: "fixed",
-        inset: 0,
-        background: "rgba(0,0,0,0.7)",
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <div
-        onClick={(e) => e.stopPropagation()}
-        style={{
-          background: "white",
-          padding: 20,
-          borderRadius: 10,
-        }}
-      >
+    <div className="modal-overlay" onClick={close}>
+      <div className="modal-box" onClick={(e) => e.stopPropagation()}>
+        
+        <button className="modal-close" onClick={close}>
+          ✕
+        </button>
+
         {children}
 
-        <button onClick={close}>Fermer</button>
       </div>
     </div>,
     document.body
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -17,13 +17,35 @@ export default function Home() {
       <Hero openModal={() => setOpen(true)} />
 
       <Modal open={open} close={() => setOpen(false)}>
-        {githubData && (
-          <>
-            <h2>{githubData.name}</h2>
-            <p>Repos publics : {githubData.public_repos}</p>
-            <p>Followers : {githubData.followers}</p>
-          </>
-        )}
+
+        <div>
+          <h2 className="modal-title">Mon profil GitHub</h2>
+
+          <div className="modal-divider"></div>
+
+          <div className="modal-content">
+
+            <img src={githubData.avatar_url} />
+
+            <div className="modal-info">
+              <p>Pseudo : {githubData.login}</p>
+              <p>Adresse : {githubData.location}</p>
+              <p>Description : {githubData.bio}</p>
+              <p>Repos : {githubData.public_repos}</p>
+              <p>Followers : {githubData.followers}</p>
+              <p>Following : {githubData.following}</p>
+            </div>
+
+          </div>
+
+          <div className="modal-divider"></div>
+
+          <div className="modal-footer">
+            <button onClick={() => setOpen(false)}>
+              Fermer
+            </button>
+          </div>
+        </div>  
       </Modal>
     </>
   );
